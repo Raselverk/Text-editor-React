ad82a9e4c714c52a8d779f4b7d86d59fde503a26
Ruslan <overko@windowslive.com>
2019-04-26 13:38:41 +0000

    Updates

---
+++ precache-manifest.bbbc20db300b958a82b441c93e15b4b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "12fc594867d838bcb69b",
+    "url": "/Text-editor-React/static/js/runtime~main.cff5d7d3.js"
+  },
+  {
+    "revision": "524312399f0a1f9f8365",
+    "url": "/Text-editor-React/static/js/main.f534d489.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/Text-editor-React/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "524312399f0a1f9f8365",
+    "url": "/Text-editor-React/static/css/main.4ffd598f.chunk.css"
+  },
+  {
+    "revision": "82f153e15d2620daaa0cc84a1a170803",
+    "url": "/Text-editor-React/index.html"
+  }
+];
